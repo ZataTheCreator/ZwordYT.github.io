ac495030dacc67804ac2a73926a974c94bccd0b2
ZwordYT <strom.linus@hotmail.se>
2021-07-26 21:39:19 +0200

    Första versionen av spelet

---
+++ scripts.js
@@ -0,0 +1,426 @@
+var frågetyptext
+var kategoritext
+var frågetyp
+var kategori
+
+//Array listor med alla frågor
+const vadÄrIntimt = [
+"Vad är regel nummer ett i ert förhållande?",
+"Vart vill ni helst ha sex?",
+"Var är det konstigaste stället ni haft sex?",
+"Vad är RÖDS favorit sexposition?",
+"Vad är BLÅS favorit sexposition?",
+"Vad är RÖDS favoritsätt att kyssas?",
+"Vad är BLÅS favoritsätt att kyssas?",
+"Vad är RÖDS favoritställe att bli kysst på?",
+"Vad är BLÅS favoritställe att bli kysst på?"
+];
+
+const vadÄrNöjen = [
+"Vart skulle ni vilja resa?",
+"Var skulle ni vilja äta middag?",
+"Vad skulle ni vilja äta till middag?",
+"Vilken film har ni sett flest gånger?",
+"Vad är RÖDS drömresemål?",
+"Vad är BLÅS drömresemål?",
+"Vad är ett instrument RÖDS vill lära sig spela?",
+"Vad är ett instrument BLÅS vill lära sig spela?",
+"Vad gör RÖD lycklig?",
+"Vad gör BLÅ lycklig?",
+"Vad är RÖDS favorit objekt hemma?",
+"Vad är BLÅS favorit objekt hemma?",
+"Vad är RÖDS favoritbok?",
+"Vad är BLÅS favoritbok?",
+"Vad är RÖDS favoritfilm?",
+"Vad är BLÅS favoritfilm?",
+"Vad är RÖDS favoritmat?",
+"Vad är BLÅS favoritmat?",
+"Vad är RÖDS favoritfärg?",
+"Vad är BLÅS favoritfärg?",
+"Vad är RÖDS favorit musikgenre?",
+"Vad är BLÅS favorit musikgenre?",
+"Vem är RÖDS favorit artist/band?",
+"Vem är BLÅS favorit artist/band?",
+"Vad är RÖDS favoritlåt?",
+"Vad är BLÅS favoritlåt?"
+];
+
+const vadÄrJobbHem = [
+"Var ligger ert drömhus?",
+"Vad är RÖDS drömjobb?",
+"Vad är BLÅS drömjobb?",
+"Vilken vardagssyssla hatar RÖD mest?",
+"Vilken vardagssyssla hatar BLÅ mest?",
+"Vad är RÖDS bästa köp till hemmet?",
+"Vad är BLÅS bästa köp till hemmet?",
+"Vilket jobb hade RÖD varit sämst på?",
+"Vilket jobb hade BLÅ varit sämst på?",
+"Var i hemmet spenderar RÖD mest tid?",
+"Var i hemmet spenderar BLÅ mest tid?"
+];
+
+const vadÄrDåtidFramtid = [
+"Vad är det bästa ni har gjort tillsammans?",
+"Vad är något RÖD skulle vilja återuppleva från sin barndom?",
+"Vad är något BLÅ skulle vilja återuppleva från sin barndom?",
+"Hur gammal skulle RÖDS vilja bli?",
+"Hur gammal skulle BLÅS vilja bli?",
+"Vem var RÖDS bästa barndomsvän?",
+"Vem var BLÅS bästa barndomsvän?"
+];
+
+const vadÄrPersonlighet = [
+"Vad gör att RÖD ogillar en person?",
+"Vad gör att BLÅ ogillar en person?",
+"Vad är RÖD mest stolt över gällande sig själv?",
+"Vad är BLÅ mest stolt över gällande sig själv?",
+"Vad är ett personlighetsdrag som beskriver RÖD bäst?",
+"Vad är ett personlighetsdrag som beskriver BLÅ bäst?",
+"Vad är RÖDS livsmotto?",
+"Vad är BLÅS livsmotto?",
+"Vad ger RÖDS liv mening?",
+"Vad ger BLÅS liv mening?",
+"Vilken är RÖDS favorit egenskap hos andra?",
+"Vilken är BLÅS favorit egenskap hos andra?",
+"Vilken är RÖDS favorit egenskap hos sig själv?",
+"Vilken är BLÅS favorit egenskap hos sig själv?",
+"Vad är RÖDS största atletiska svaghet?",
+"Vad är BLÅS största atletiska svaghet?"
+
+];
+
+const vadÄrBlandat = [
+"I vilken påhittad värld skulle RÖD vilja leva i?",
+];
+
+//Kombinerar alla katigorier i Vad Är? till en Array
+const allaVadÄr = [...vadÄrIntimt, ...vadÄrNöjen, ...vadÄrJobbHem, ...vadÄrDåtidFramtid, ...vadÄrPersonlighet, ...vadÄrBlandat]
+
+
+const vemÄrIntimt = [
+"Vem vill ha sex mest?",
+"Vem är snyggast?",
+"Vem är bäst i sängen?",
+"Vem ger bäst oralt?",
+"Vem är mest kinky?",
+"Vem vill kyssas mest?",
+"Vem kysser bäst?",
+"Vem vill kramas mest?",
+"Vem kramas bäst?",
+"Vem sa ''Jag älskar dig'' först ?",
+"Vem tänker mest på sex?",
+"Vem är snyggast naken?"
+];
+
+const vemÄrNöjen = [
+"Vem är mest musikalisk?",
+"Vem kollar på mest film?"
+
+];
+
+const vemÄrJobbHem = [
+"Vem föredrar att resa?",
+"Vem stökar ner mest?",
+"Vem gör mest hemmasysslor?",
+"Vem slösar mest pengar?"
+
+];
+
+const vemÄrDåtidFramtid = [
+"Vem hade best uppväxt?",
+"Vem var jobbigast som barn?",
+"Vem har störst planer?",
+"Vem har störst chans att bli rik?"
+
+];
+
+const vemÄrPersonlighet = [
+"Vad är erat livsmotto?",
+"Vem är mest självsäker?",
+"Vem är mest materialistisk?",
+"Vem är mest känslosam?",
+"Vem är dummast?",
+"Vem är klantigast?",
+"Vem stressar mest?",
+"Vem är nördigast?",
+"Vem blir lättare irriterad?"
+
+];
+
+const vemÄrBlandat = [
+"Vem fiser mest?",
+"Vem rapar mest?",
+"Vem är villig att mörda för minst pengar?",
+"Vem har flest hemligheter?",
+"Vem dansar mest?",
+"Vems föräldrar hade vunnit i ett slagsmål mot varandra?",
+"Vem litar minst på andra människor?",
+"Vem kör snabbast?"
+
+];
+
+//Kombinerar alla katigorier i Vem Är? till en Array
+const allaVemÄr = [...vemÄrIntimt, ...vemÄrNöjen, ...vemÄrJobbHem, ...vemÄrDåtidFramtid, ...vemÄrPersonlighet, ...vemÄrBlandat]
+
+
+function FrågaRelationerFunc() {
+	
+	//Slumpar en typ av fråga
+	frågetyp=Math.floor(Math.random() * 3);
+	
+	if (frågetyp === 0) {
+		frågetyptext = "Vad Är?";}
+	
+	else if (frågetyp === 1) {
+		frågetyptext = "Vem Är?";}
+	
+	else if (frågetyp === 2) {
+		frågetyptext = "Alla Svarar!";}
+
+	//Ändra texten på skärmen så den visar rätt frågetyp
+	document.getElementById("frågetyptext").innerHTML = frågetyptext;
+	
+	//Slumpar en kategori
+	kategori=Math.floor(Math.random() * 6);
+	
+	if (kategori === 0) {
+		kategoritext = "Intimt";}
+		
+	else if (kategori === 1) {
+		kategoritext = "Nöjen";}
+	
+	else if (kategori === 2) {
+		kategoritext = "Jobb & Hem";}
+	
+	else if (kategori === 3) {
+		kategoritext = "Dåtid & Framtid";}
+	
+	else if (kategori === 4) {
+		kategoritext = "Personlighet";}
+	
+	else if (kategori === 5) {
+		kategoritext = "Blandat";}
+
+	//Ändra texten på skärmen så den visar rätt kategori
+	document.getElementById("kategoritext").innerHTML = kategoritext;
+
+	NyFrågaRelationerFunc();
+}
+
+function NyFrågaRelationerFunc() {
+	//Slumpar fram frågan till Vad Är? och Alla Svarar!
+	if (frågetyp === 0 || frågetyp === 2) {
+		
+		//Intimt
+		if (kategori === 0) {
+			var frågaNr=Math.floor(Math.random() * vadÄrIntimt.length);
+			document.getElementById("frågetext").innerHTML = vadÄrIntimt[frågaNr];}	
+		
+		//Nöjen
+		else if (kategori === 1) {
+			var frågaNr=Math.floor(Math.random() * vadÄrNöjen.length);
+			document.getElementById("frågetext").innerHTML = vadÄrNöjen[frågaNr];}
+		
+		//Jobb & Hem
+		else if (kategori === 2) {
+			var frågaNr=Math.floor(Math.random() * vadÄrJobbHem.length);
+			document.getElementById("frågetext").innerHTML = vadÄrJobbHem[frågaNr];}
+	
+		//Dåtid & Framtid
+		else if (kategori === 3) {
+			var frågaNr=Math.floor(Math.random() * vadÄrDåtidFramtid.length);
+			document.getElementById("frågetext").innerHTML = vadÄrDåtidFramtid[frågaNr];}
+	
+		//Personlighet
+		else if (kategori === 4) {
+			var frågaNr=Math.floor(Math.random() * vadÄrPersonlighet.length);
+			document.getElementById("frågetext").innerHTML = vadÄrPersonlighet[frågaNr];}
+	
+		//Blandat
+		else if (kategori === 5) {
+			var frågaNr=Math.floor(Math.random() * vadÄrBlandat.length);
+			document.getElementById("frågetext").innerHTML = vadÄrBlandat[frågaNr];}
+	}
+	
+	//Slumpar fram frågan till Vem Är?
+	else if (frågetyp === 1) {
+			
+		//Intimt
+		if (kategori === 0) {
+			var frågaNr=Math.floor(Math.random() * vemÄrIntimt.length);
+			document.getElementById("frågetext").innerHTML = vemÄrIntimt[frågaNr];}	
+		
+		//Nöjen
+		else if (kategori === 1) {
+			var frågaNr=Math.floor(Math.random() * vemÄrNöjen.length);
+			document.getElementById("frågetext").innerHTML = vemÄrNöjen[frågaNr];}
+		
+		//Jobb & Hem
+		else if (kategori === 2) {
+			var frågaNr=Math.floor(Math.random() * vemÄrJobbHem.length);
+			document.getElementById("frågetext").innerHTML = vemÄrJobbHem[frågaNr];}
+	
+		//Dåtid & Framtid
+		else if (kategori === 3) {
+			var frågaNr=Math.floor(Math.random() * vemÄrDåtidFramtid.length);
+			document.getElementById("frågetext").innerHTML = vemÄrDåtidFramtid[frågaNr];}
+	
+		//Personlighet
+		else if (kategori === 4) {
+			var frågaNr=Math.floor(Math.random() * vemÄrPersonlighet.length);
+			document.getElementById("frågetext").innerHTML = vemÄrPersonlighet[frågaNr];}
+	
+		//Blandat
+		else if (kategori === 5) {
+			var frågaNr=Math.floor(Math.random() * vemÄrBlandat.length);
+			document.getElementById("frågetext").innerHTML = vemÄrBlandat[frågaNr];}	
+	}
+	
+	
+}
+
+
+function FrågaVännerFunc() {
+	
+	//Slumpar en typ av fråga
+	frågetyp=Math.floor(Math.random() * 3);
+	
+	if (frågetyp === 0) {
+		frågetyptext = "Vad Är?";}
+	
+	else if (frågetyp === 1) {
+		frågetyptext = "Vem Är?";}
+	
+	else if (frågetyp === 2) {
+		frågetyptext = "Alla Svarar!";}
+
+	//Ändra texten på skärmen så den visar rätt frågetyp
+	document.getElementById("frågetyptext").innerHTML = frågetyptext;
+	
+	//Slumpar en kategori
+	kategori=Math.floor(Math.random() * 5);
+		
+	if (kategori === 0) {
+		kategoritext = "Nöjen";}
+	
+	else if (kategori === 1) {
+		kategoritext = "Jobb & Hem";}
+	
+	else if (kategori === 2) {
+		kategoritext = "Dåtid & Framtid";}
+	
+	else if (kategori === 3) {
+		kategoritext = "Personlighet";}
+	
+	else if (kategori === 4) {
+		kategoritext = "Blandat";}
+
+	//Ändra texten på skärmen så den visar rätt kategori
+	document.getElementById("kategoritext").innerHTML = kategoritext;
+
+	NyFrågaVännerFunc();
+}
+
+function NyFrågaVännerFunc() {
+	//Slumpar fram frågan till Vad Är? och Alla Svarar!
+	if (frågetyp === 0 || frågetyp === 2) {
+		
+		//Nöjen
+		if (kategori === 0) {
+			var frågaNr=Math.floor(Math.random() * vadÄrNöjen.length);
+			document.getElementById("frågetext").innerHTML = vadÄrNöjen[frågaNr];}
+		
+		//Jobb & Hem
+		else if (kategori === 1) {
+			var frågaNr=Math.floor(Math.random() * vadÄrJobbHem.length);
+			document.getElementById("frågetext").innerHTML = vadÄrJobbHem[frågaNr];}
+	
+		//Dåtid & Framtid
+		else if (kategori === 2) {
+			var frågaNr=Math.floor(Math.random() * vadÄrDåtidFramtid.length);
+			document.getElementById("frågetext").innerHTML = vadÄrDåtidFramtid[frågaNr];}
+	
+		//Personlighet
+		else if (kategori === 3) {
+			var frågaNr=Math.floor(Math.random() * vadÄrPersonlighet.length);
+			document.getElementById("frågetext").innerHTML = vadÄrPersonlighet[frågaNr];}
+	
+		//Blandat
+		else if (kategori === 4) {
+			var frågaNr=Math.floor(Math.random() * vadÄrBlandat.length);
+			document.getElementById("frågetext").innerHTML = vadÄrBlandat[frågaNr];}
+	}
+	
+	//Slumpar fram frågan till Vem Är?
+	else if (frågetyp === 1) {
+			
+		//Nöjen
+		if (kategori === 0) {
+			var frågaNr=Math.floor(Math.random() * vemÄrNöjen.length);
+			document.getElementById("frågetext").innerHTML = vemÄrNöjen[frågaNr];}
+		
+		//Jobb & Hem
+		else if (kategori === 1) {
+			var frågaNr=Math.floor(Math.random() * vemÄrJobbHem.length);
+			document.getElementById("frågetext").innerHTML = vemÄrJobbHem[frågaNr];}
+	
+		//Dåtid & Framtid
+		else if (kategori === 2) {
+			var frågaNr=Math.floor(Math.random() * vemÄrDåtidFramtid.length);
+			document.getElementById("frågetext").innerHTML = vemÄrDåtidFramtid[frågaNr];}
+	
+		//Personlighet
+		else if (kategori === 3) {
+			var frågaNr=Math.floor(Math.random() * vemÄrPersonlighet.length);
+			document.getElementById("frågetext").innerHTML = vemÄrPersonlighet[frågaNr];}
+	
+		//Blandat
+		else if (kategori === 4) {
+			var frågaNr=Math.floor(Math.random() * vemÄrBlandat.length);
+			document.getElementById("frågetext").innerHTML = vemÄrBlandat[frågaNr];}	
+	}
+}
+
+
+function FrågaAllaFunc() {
+	
+	//Slumpar fårgetyp
+	frågetyp=Math.floor(Math.random() * 3);
+	
+	if (frågetyp === 0) {
+		frågetyptext = "Vad Är?";}
+	
+	else if (frågetyp === 1) {
+		frågetyptext = "Vem Är?";}
+	
+	else if (frågetyp === 2) {
+		frågetyptext = "Alla Svarar!";}
+
+	//Ändra texten på skärmen så den visar rätt frågetyp
+	document.getElementById("frågetyptext").innerHTML = frågetyptext;
+	
+	NyFrågaAllaFunc();
+}
+
+function NyFrågaAllaFunc() {
+
+	//Slumpar fram kategori
+	kategori=Math.floor(Math.random() * 6);
+	
+	//Slumpar fram frågan till Vad Är? och Alla Svarar!
+	if (frågetyp === 0 || frågetyp === 2) {
+		
+			var frågaNr=Math.floor(Math.random() * allaVadÄr.length);
+			document.getElementById("frågetext").innerHTML = allaVadÄr[frågaNr];		
+	}
+	
+	//Slumpar fram frågan till Vem Är?
+	else if (frågetyp === 1) {
+			
+			var frågaNr=Math.floor(Math.random() * allaVemÄr.length);
+			document.getElementById("frågetext").innerHTML = allaVemÄr[frågaNr];
+		
+	}
+	
+	
+}
